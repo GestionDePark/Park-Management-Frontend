22957a1ea467352922007929c557f096a58cf1d5
Harenabs21 <hei.harena.6@gmail.com>
2024-02-03 16:43:53 +0300

    feat: add router and import tailwind

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Form from './components/pages/Login/Form'
 
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />,
+},{
+  path: '/form',
+  element: <Form />
+}])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router} / >
   </React.StrictMode>
 );
 
