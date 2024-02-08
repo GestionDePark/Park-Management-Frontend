61a660b3ef35e41a80f0b6fa69332d18015a4aa4
kirisaki_vk <kirisaki.vk@gmail.com>
2024-02-08 10:52:24 +0300

    feat: login page

---
+++ src/pages/Login/ForgotPasswordDialog.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { TransitionProps } from '@mui/material/transitions';
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export function ForgotPasswordDialog(props: DialogProps & {
+  closeHandler: () => void;
+}) {
+
+  return (
+    <>
+      <Dialog
+        {...props}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Forgot Password ?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Please contact your administrator to restore your password
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={props.closeHandler}>Ok</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
