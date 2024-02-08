4544e55a7f62f218146139eaa0a00de41145be56
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-08 15:03:26 +0300

    feat: use getAuthorization()

--- src/api/employee.ts
+++ src/api/employee.ts
@@ -1,10 +1,20 @@
 import fetcher from '@/api/fetcher.ts';
+import { EmployeeData } from '@/api/types.ts';
+import getAuthorization from '@/utils/getAuthorization.ts';
 
 class Employee {
-  public static async delete(id: string, adminToken: string) {
+  public static async delete(id: string): Promise<EmployeeData> {
     return (
       await fetcher.delete(`/employee/${encodeURIComponent(id)}`, {
-        headers: { Authorization: 'Bearer ' + adminToken },
+        headers: getAuthorization(),
+      })
+    ).data;
+  }
+
+  public static async findAll(): Promise<EmployeeData[]> {
+    return (
+      await fetcher.get('/employee', {
+        headers: getAuthorization(),
       })
     ).data;
   }
