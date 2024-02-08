3aba255ffa204e16fb5e2ce23e7e001ef5a62cda
Harenabs21 <hei.harena.6@gmail.com>
2024-02-08 22:29:55 +0300

    feat: integrate the footer to the homepage

--- src/pages/Visitor/Path.tsx
+++ src/pages/Visitor/Path.tsx
@@ -1,12 +1,14 @@
 import Navli from './Navli';
 import FaceBody from './Container/Facebody';
 import Container from './Container/Container';
+import Facefoot from './Container/Facefoot';
 const Path = () => {
   return (
     <>
       <Navli />
       <FaceBody />
       <Container />
+      <Facefoot />
     </>
   );
 };
