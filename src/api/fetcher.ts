f0d5cff1296eaba88442c53ff49b3bb810f42041
ToavinaMaria <ToavinaMaria>
2024-02-08 11:37:04 +0300

    style: reformat

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import axios from 'axios';
 
 const fetcher = axios.create({
@@ -10,3 +11,17 @@ const fetcher = axios.create({
 });
 
 export default fetcher;
+=======
+import axios from 'axios';
+
+const fetcher = axios.create({
+  baseURL: !import.meta.env.IS_DEV
+    ? 'https://park-api.up.railway.app'
+    : 'http://localhost:8080',
+  headers: {
+    Accept: '*/*',
+  },
+});
+
+export default fetcher;
+>>>>>>> 384ffe9 (style: reformat)
