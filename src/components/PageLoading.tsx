2e439851666150bceba42b6b0e71164866634559
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-08 10:52:03 +0300

    feat: animated loading

--- src/components/PageLoading.tsx
+++ src/components/PageLoading.tsx
@@ -1,5 +1,13 @@
+import { TreeLoading } from './Lottie';
+
 const PageLoading = () => {
-  return null;
+  return (
+    <div className="w-screen h-screen center-flex">
+      <div className="center-flex w-[50%] h-[50%]">
+        <TreeLoading />
+      </div>
+    </div>
+  );
 };
 
 export default PageLoading;
