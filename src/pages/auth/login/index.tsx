edf86592fed862de7fc37af211abb3d73377ed01
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-08 11:14:28 +0300

    feat: style login

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,12 +1,15 @@
 import { Typography } from '@mui/material';
 import FormLogin from '@/pages/auth/login/FormLogin.tsx';
+import { TreeLoading } from '@/components/Lottie';
 
 const Login = () => {
   return (
     <div className="w-screen h-screen flex">
-      <div className="w-full flex-shrink"></div>
+      <div className="w-full flex-shrink">
+        <TreeLoading />
+      </div>
 
-      <div className="flex h-full w-full py-8">
+      <div className="center-flex h-full w-full py-8">
         <div className="p-8 flex flex-col gap-12 h-full">
           <div className="center-flex flex-col">
             <Typography variant="h3">Welcome back !</Typography>
