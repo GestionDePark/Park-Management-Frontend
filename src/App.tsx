6dc2a97d9352823c9fdb8055bb62a984c7022a83
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-08 09:27:35 +0300

    feat: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { ThemeProvider } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import muiTheme from '@/core/muiTheme.ts';
+import pageRoutes from '@/pageRoutes.ts';
+import Dashboard from '@/pages/Admin/Dashboard';
+import Employee from '@/pages/Admin/Employee';
+import SecureRoute from '@/layer/SecureRoute.tsx';
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={muiTheme('light')}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path={pageRoutes.adminDashboard}
+            Component={SecureRoute(Dashboard, ['admin'])}
+          />
+          <Route
+            path={pageRoutes.adminEmployee}
+            Component={SecureRoute(Employee, ['admin'])}
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
