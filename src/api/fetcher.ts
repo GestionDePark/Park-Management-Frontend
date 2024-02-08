bbde8855fd48a016e122a1921bced897f2ca6470
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-08 15:18:01 +0300

    fix: resolve nested code

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 const fetcher = axios.create({
@@ -11,17 +10,3 @@ const fetcher = axios.create({
 });
 
 export default fetcher;
-=======
-import axios from 'axios';
-
-const fetcher = axios.create({
-  baseURL: !import.meta.env.IS_DEV
-    ? 'https://park-api.up.railway.app'
-    : 'http://localhost:8080',
-  headers: {
-    Accept: '*/*',
-  },
-});
-
-export default fetcher;
->>>>>>> 384ffe9 (style: reformat)
--- src/components/PageLoading.tsx
+++ src/components/PageLoading.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { TreeLoading } from './Lottie';
 
 const PageLoading = () => {
@@ -12,18 +11,3 @@ const PageLoading = () => {
 };
 
 export default PageLoading;
-=======
-import { TreeLoading } from './Lottie';
-
-const PageLoading = () => {
-  return (
-    <div className="w-screen h-screen center-flex">
-      <div className="center-flex w-[50%] h-[50%]">
-        <TreeLoading />
-      </div>
-    </div>
-  );
-};
-
-export default PageLoading;
->>>>>>> 384ffe9 (style: reformat)
