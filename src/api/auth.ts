cddf8e81f5ac3b2bc7abd2bc6138d0c0182802b6
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-14 11:52:38 +0300

    refactor: move files

--- src/api/auth.ts
+++
@@ -1,8 +0,0 @@
-import { LoginData, LoginResponse } from '@/services/auth/types.ts';
-import fetcher from '@/api/fetcher.ts';
-
-const getToken = async (data: LoginData): Promise<LoginResponse> => {
-  return (await fetcher.post('/auth/token', data)).data;
-};
-
-export default getToken;
