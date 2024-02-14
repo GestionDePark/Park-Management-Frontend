bca4633d4a99ea6ad3b69b4b374cc427b72396d1
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-14 12:02:07 +0300

    feat: USE_REMOTE env

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const fetcher = axios.create({
-  baseURL: !import.meta.env.DEV
-    ? 'https://park-api.up.railway.app'
-    : 'http://localhost:8080',
+  baseURL:
+    !import.meta.env.DEV || import.meta.env.USE_REMOTE
+      ? 'https://park-api.up.railway.app'
+      : 'http://localhost:8080',
   headers: {
     Accept: '*/*',
   },
