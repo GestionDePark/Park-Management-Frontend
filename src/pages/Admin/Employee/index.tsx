89d58bdcff52800c12d561404a1f87fa9c6f8e96
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-17 20:51:08 +0300

    feat: dialog integrations

--- src/pages/Admin/Employee/index.tsx
+++ src/pages/Admin/Employee/index.tsx
@@ -1,42 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Button } from '@mui/material';
 import TableEmployee from '@/pages/Admin/components/TableEmployee';
-import { useEffect, useRef, useState } from 'react';
 import EmployeeApi from '@/api/providers/EmployeeProvider';
 import { EmployeeData } from '@/api/types';
 import useErrorPopup from '@/hooks/useErrorPopup';
 import { Dashy } from '../Dashy';
-import { AddEmployeeDialog } from '@/pages/Admin/Employee/AddEmployeeDialog';
+import { AddEmployeeDialog } from './AddEmployeeDialog';
+import { UpdateEmployeeDialog } from '@/pages/Admin/Employee/UpdateEmployeeDialog';
 
 const Employee = () => {
-  const renderIncr = useRef(0);
-  const countFetch = useRef(0);
+  const [renderFirst, setRenderFirst] = useState(true);
   const [errorNode, setErrorNode] = useErrorPopup();
-
   const [selected, setSelected] = useState<number[]>([]);
-
   const [data, setData] = useState<EmployeeData[]>([]);
 
   const [openAddDialog, setOpenAddDialog] = useState(false);
+  const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
 
   const fetchData = async () => {
     try {
-      if (data.length === 0 && countFetch.current < 4) {
+      if (data.length === 0) {
         setData(await EmployeeApi.findAll());
       }
     } catch (e) {
       setErrorNode(e as Error);
     }
-    if (countFetch.current <= 4) {
-      countFetch.current++;
-    }
   };
 
   useEffect(() => {
-    if (renderIncr.current === 1) {
+    if (renderFirst) {
+      setRenderFirst(false);
       fetchData();
-    }
-    if (renderIncr.current < 2) {
-      renderIncr.current++;
     }
   });
 
@@ -73,7 +67,9 @@ const Employee = () => {
       case 1:
         return (
           <div className="center-flex gap-2">
-            <Button variant="outlined">Modify</Button>
+            <Button variant="outlined" onClick={handleUpdateClick}>
+              Modify
+            </Button>
             <Button variant="outlined" onClick={handleDelete}>
               Delete
             </Button>
@@ -88,35 +84,56 @@ const Employee = () => {
     }
   };
 
+  const handleDataAdded = (data: EmployeeData) => {
+    setData((p) => p.concat(data));
+  };
+
+  const handleUpdateClick = () => {
+    setOpenUpdateDialog((p) => !p);
+  };
+
+  const handleCloseAddDial = () => {
+    setOpenAddDialog(false);
+  };
+
+  const handleCloseUpdateDial = () => {
+    setOpenUpdateDialog(false);
+  };
+
+  const handleModified = (i: number, data: EmployeeData) => {
+    setData((p) => {
+      p[i] = data;
+      return p;
+    });
+  };
+
   return (
     <Dashy>
       <div className="flex justify-between p-2">
         <span className="text-xl font-semibold">Manage your employees</span>
-
         <div>
           <SwitchBtnFeatures />
         </div>
       </div>
+
       <TableEmployee data={data} onSelect={handleSelect} />
-      {errorNode}
-      <AddEmployeeDialog
-        open={openAddDialog}
-        onClose={() => {
-          setOpenAddDialog(false);
-        }}
-        fullWidth
-        PaperProps={{
-          component: 'form',
-          onSubmit: {},
-          sx: {
-            borderRadius: '10px',
-            minWidth: '420px',
-          },
-        }}
-        onCloseClick={() => {
-          setOpenAddDialog(false);
-        }}
+
+      <UpdateEmployeeDialog
+        onModified={handleModified}
+        open={openUpdateDialog}
+        onClose={handleCloseUpdateDial}
+        onCloseClick={handleCloseUpdateDial}
+        updateIndex={selected[0] >= 0 ? selected[0] : -1}
+        employeeData={selected[0] >= 0 ? data[selected[0]] : null}
       />
+
+      <AddEmployeeDialog
+        onAddedData={handleDataAdded}
+        open={openAddDialog}
+        onClose={handleCloseAddDial}
+        onCloseClick={handleCloseAddDial}
+      />
+      {errorNode}
     </Dashy>
   );
 };
