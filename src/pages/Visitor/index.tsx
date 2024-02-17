f34b868a18ad0d164d888fd45e84f5b02add8f81
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-17 13:36:02 +0300

    refactor: change visitor

---
+++ src/pages/Visitor/index.tsx
@@ -0,0 +1,16 @@
+import Navli from './Navli';
+import FaceBody from './Container/Facebody';
+import Container from './Container/Container';
+import Facefoot from './Container/Facefoot';
+
+const Path = () => {
+  return (
+    <div>
+      <Navli />
+      <FaceBody />
+      <Container />
+      <Facefoot />
+    </div>
+  );
+};
+export default Path;
