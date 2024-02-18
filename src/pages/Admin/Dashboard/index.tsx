8a2fe73780b9e3722486dcefe0c7419b7afe9754
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-18 12:17:22 +0300

    feat: optimize fetching

--- src/pages/Admin/Dashboard/index.tsx
+++ src/pages/Admin/Dashboard/index.tsx
@@ -1,32 +1,28 @@
 import { Dashy } from '../Dashy';
 import { Paper } from '@mui/material';
 import SampleEmployee from '@/pages/Admin/Dashboard/SampleEmployee';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { EmployeeData } from '@/api/types/Employee.type';
 import EmployeeApi from '@/api/providers/EmployeeProvider';
 import useErrorPopup from '@/hooks/useErrorPopup';
 import AppUsesContext from '@/components/AppUsesContext';
 
 const Dashboard = () => {
-  const countFetch = useRef(0);
+  const [firstRender, setFirstRender] = useState(true);
   const [data, setData] = useState<EmployeeData[]>([]);
   const [errorNode, setErrorNode] = useErrorPopup();
 
   const fetchData = async () => {
-    try {
-      if (data.length === 0 && countFetch.current < 4) {
-        setData(await EmployeeApi.findAll());
-      }
-    } catch (e) {
-      setErrorNode(e as Error);
-    }
-    if (countFetch.current <= 4) {
-      countFetch.current++;
+    if (data.length === 0) {
+      setData(await EmployeeApi.findAll());
     }
   };
 
   useEffect(() => {
-    fetchData();
+    if (firstRender) {
+      setFirstRender(false);
+      fetchData().catch(setErrorNode);
+    }
   });
 
   return (
