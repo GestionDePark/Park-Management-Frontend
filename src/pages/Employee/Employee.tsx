f494b232cbe9b7f3227f0ec31539881ce4f53646
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-02-18 10:49:04 +0300

    chore: clean component

--- src/pages/Employee/Employee.tsx
+++ src/pages/Employee/Employee.tsx
@@ -1,25 +1,9 @@
 import Header from '@/components/Header';
-import { useEffect, useState } from 'react';
-import { UserData } from '@/services/auth/types';
-import UserProvider from '@/api/providers/UserProvider';
-import useErrorPopup from '@/hooks/useErrorPopup';
 
 const Employee = () => {
-  const [firstRender, setFirstRender] = useState(true);
-  const [errorNode, setErrorNode] = useErrorPopup();
-  const [currentUser, setUser] = useState<UserData | null>(null);
-
-  useEffect(() => {
-    if (firstRender) {
-      setFirstRender(false);
-      UserProvider.findSelf().then(setUser).catch(setErrorNode);
-    }
-  });
-
   return (
     <div>
       <Header />
-      {errorNode}
     </div>
   );
 };
